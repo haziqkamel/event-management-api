1b5b78a94d6569cc48427f1bd0ee32fa675435e7
Haziq Kamel <haziqkamel@outlook.com>
2024-02-12 10:44:41 +0800

    Add pagination to attendee index endpoint

--- app/Http/Controllers/Api/AttendeeController.php
+++ app/Http/Controllers/Api/AttendeeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\AttendeeResource;
+use App\Models\Event;
 use Illuminate\Http\Request;
 
 class AttendeeController extends Controller
@@ -10,9 +12,29 @@ class AttendeeController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Event $event, Request $request)
     {
-        //
+        $perPage = $request->query("per_page");
+
+        $attendees = $event->attendees()->latest()->paginate($perPage ?? 10);
+        // Return response json that include pagination meta
+        return response()->json([
+            "status" => "success",
+            "message" => "Attendees of the event",
+            "data" => AttendeeResource::collection($attendees),
+            "meta" => [
+                "total" => $attendees->total(),
+                "per_page" => $attendees->perPage(),
+                "current_page" => $attendees->currentPage(),
+                "last_page" => $attendees->lastPage(),
+                "from" => $attendees->firstItem(),
+                "to" => $attendees->lastItem(),
+                "prev_page_url" => $attendees->previousPageUrl(),
+                "next_page_url" => $attendees->nextPageUrl(),
+                "first_page_url" => $attendees->url(1),
+                "last_page_url" => $attendees->url($attendees->lastPage()),
+            ]
+        ]);
     }
 
     /**
