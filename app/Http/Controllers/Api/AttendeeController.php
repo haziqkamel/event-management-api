85492bfb4aefd0d88c67ad5cf0c95bafab5c342f
Haziq Kamel <haziqkamel@outlook.com>
2024-02-12 11:11:00 +0800

    Add Attendee CRUD operations to AttendeeController

--- app/Http/Controllers/Api/AttendeeController.php
+++ app/Http/Controllers/Api/AttendeeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\AttendeeResource;
+use App\Models\Attendee;
 use App\Models\Event;
 use Illuminate\Http\Request;
 
@@ -40,17 +41,29 @@ class AttendeeController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request, Event $event)
     {
-        //
+        $attendee = $event->attendees()->create([
+            "user_id" => 1, //TODO: replace with authenticated user id
+        ]);
+
+        return response()->json([
+            "status" => "success",
+            "message" => "Attendee created",
+            "data" => new AttendeeResource($attendee),
+        ]);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Event $event, Attendee $attendee)
     {
-        //
+        return response()->json([
+            "status" => "success",
+            "message" => "Attendee of the event",
+            "data" => new AttendeeResource($attendee),
+        ]);
     }
 
     /**
@@ -64,8 +77,9 @@ class AttendeeController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Event $event, Attendee $attendee)
     {
-        //
+        $attendee->delete();
+        return response(status: 204);
     }
 }
