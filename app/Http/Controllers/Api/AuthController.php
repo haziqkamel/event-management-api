dea5343cc606f1ccfbee7e867ac1bb8aff6d811b
Haziq Kamel <haziqkamel@outlook.com>
2024-02-13 14:34:31 +0800

    Add AuthController and login/logout functionality

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        try {
+            $request->validate([
+                'email' => 'required|email',
+                'password' => 'required',
+            ]);
+
+            $user = User::where('email', $request->email)->first();
+
+            if (!$user || !Hash::check($request->password, $user->password)) {
+                throw ValidationException::withMessages([
+                    'error' => 'The provided credentials are incorrect.',
+                ]);
+            }
+
+            $token = $user->createToken($request->email)->plainTextToken;
+            return response()->json(['token' => $token]);
+        } catch (ValidationException $e) {
+            return response()->json([
+                "status" => "failed",
+                "message" => $e->errors()
+            ], 401);
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+}
