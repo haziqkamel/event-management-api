c9e5566fa1c95dc28f5eb8b54714000f09eff79d
Haziq Kamel <haziqkamel@outlook.com>
2024-02-13 17:01:40 +0800

    Update logout functionality and routes

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -38,7 +38,7 @@ class AuthController extends Controller
 
     public function logout(Request $request)
     {
-        $request->user()->currentAccessToken()->delete();
+        $request->user()->tokens()->delete();
         return response()->json(['message' => 'Successfully logged out']);
     }
 }
