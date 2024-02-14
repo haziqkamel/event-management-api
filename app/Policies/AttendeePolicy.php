341021a5f85314a4c997ed3f0ebae96fdfc280be
Haziq Kamel <haziqkamel@outlook.com>
2024-02-14 14:00:32 +0800

    Add AttendeePolicy and EventPolicy classes***
    ***Add AttendeePolicy and EventPolicy classes to handle authorization for viewing, creating, and deleting models.

---
+++ app/Policies/AttendeePolicy.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Attendee;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class AttendeePolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(?User $user): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(?User $user, Attendee $attendee): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Attendee $attendee): Response
+    {
+        return ($attendee->event->user->id === $user->id ||
+            $attendee->user->id === $user->id) ?
+            Response::allow() :
+            Response::deny("You do not own this attendee.");
+    }
+}
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,12 +25,6 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Gate::define("update-event", function ($user, Event $event) {
-            return $event->user->id === $user->id;
-        });
-
-        Gate::define("delete-attendee", function ($user, Event $event, Attendee $attendee) {
-            return $event->user->id === $user->id || $attendee->user->id === $user->id;
-        });
+        //
     }
 }
